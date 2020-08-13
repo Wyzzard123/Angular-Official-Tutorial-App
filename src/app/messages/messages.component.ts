e04634944aea17d97d8181bd285625dc6e640425
Louiz Kim-Chan <kimblader151@yahoo.com.sg>
2020-08-13 16:52:02 +0800

    Added Message Service

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-messages',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  // messageService must be public as we are going to bind it in the template
+  constructor(public messageService: MessageService) { }
 
   ngOnInit(): void {
   }
