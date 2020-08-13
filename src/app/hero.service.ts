57108c86669a623eaa6717a036028531bd6e8a5d
Louiz Kim-Chan <kimblader151@yahoo.com.sg>
2020-08-13 19:43:27 +0800

    Added delete method.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -36,7 +36,7 @@ export class HeroService {
     const url = `${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
-      catchError(this.handleError<Hero>(`getHero id=${id}`))
+      catchError(this.handleError<Hero>(`getHero`))
     );
   }
 
@@ -71,8 +71,21 @@ export class HeroService {
   /** POST: add a new hero to the server */
   addHero(hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
-      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
-      catchError(this.handleError<Hero>('addHero'))
+      tap(newHero => this.log(`Added hero with id=${newHero.id}`)),
+      catchError(this.handleError<any>('addHero'))
     );
   }
+
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${hero.id}`)),
+      catchError(this.handleError<any>('deleteHero'))
+    );
+  }
+
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,10 +11,11 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
+
   constructor(
     private heroService: HeroService,
-    private messageService: MessageService)
-  { }
+    private messageService: MessageService) {
+  }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -38,7 +39,19 @@ export class HeroesComponent implements OnInit {
     this.heroService.addHero({name} as Hero)
       .subscribe(hero => {
         this.heroes.push(hero);
-    });
+      });
 
+  }
+
+  /* If you neglect to subscribe(), the service will not send the delete request to the server.
+     As a rule, an Observable does nothing until something subscribes.
+
+     Confirm this for yourself by temporarily removing the subscribe(),
+     clicking "Dashboard", then clicking "Heroes".
+     You'll see the full list of heroes again.*/
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 }
